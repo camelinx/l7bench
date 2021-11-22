8a7614b04ffc9cc5a50a774b351dfdd744a8286b
Sudarsan Raghavan <camelinx@gmail.com>
2021-11-22 09:16:03 -0800

    Support for setting req body size and type. Fixed variable naming convention

--- cmd/l7bench/l7bench.go
+++ cmd/l7bench/l7bench.go
@@ -17,13 +17,15 @@ var (
     port                = flag.String( "test-port", "", "Port serving traffic. Optional and defaults to 80 or 443" )
     method              = flag.String( "test-method", "GET", "HTTP request method. Optional and defaults to GET" )
     url                 = flag.String( "test-url", "/", "HTTP request url. Optional and defaults to /" )
-    concurrent_conns    = flag.Uint( "concurrent-conns", uint( 50 ), "Concurrent connections to maintain with the server. Defaults to 50" )
+    reqBodySize         = flag.Uint( "test-body-size", uint( 0 ), "HTTP request body size. Optional and defaults to 0 (no request body)" )
+    reqBodyType         = flag.String( "test-body-type", "plain", "HTTP request body type \"plain\" or \"json\". Optional and defaults to plain text" )
+    concurrentConns     = flag.Uint( "concurrent-conns", uint( 50 ), "Concurrent connections to maintain with the server. Defaults to 50" )
     duration            = flag.Duration( "duration", 5 * time.Minute, "Total duration of the test. Defaults to 5 minutes" )
     conn_reqs           = flag.Uint( "conn-requests", uint( 2 ), "Number of requests to send in a TCP connection. Defaults to 2" )
-    conn_req_intvl      = flag.Duration( "conn-req-interval", 1 * time.Second, "Interval in between requests on the same TCP connection. Defaults to 1 second" )
-    idle_timeout        = flag.Duration( "idle-timeout", 30 * time.Second, "Timeout after which test will give up on an unresponsive server. Defaults to 30 seconds" )
+    connReqIntvl        = flag.Duration( "conn-req-interval", 1 * time.Second, "Interval in between requests on the same TCP connection. Defaults to 1 second" )
+    idleTimeout         = flag.Duration( "idle-timeout", 30 * time.Second, "Timeout after which test will give up on an unresponsive server. Defaults to 30 seconds" )
     secure              = flag.Bool( "secure", false, "Send requests to TLS enabled server. Defaults to false" )
-    disable_server_auth = flag.Bool( "disable-server-auth", false, "Disable TLS server authentication. Defaults to false" )
+    disableServerAuth   = flag.Bool( "disable-server-auth", false, "Disable TLS server authentication. Defaults to false" )
 )
 
 func main( ) {
@@ -46,8 +48,14 @@ func main( ) {
     setupString( &httpBench.Method, method, "L7BENCH_TEST_METHOD" )
     setupString( &httpBench.Url, url, "L7BENCH_TEST_URL" )
 
+    setupUint( &httpBench.ReqBodySize, reqBodySize, "L7BENCH_TEST_REQ_BODY_SIZE" )
+
+    var finalReqBodyType string
+    setupString( &finalReqBodyType, reqBodyType, "L7BENCH_TEST_REQ_BODY_TYPE" )
+    httpBench.SetupHttpBenchReqBodyType( finalReqBodyType )
+
     setupBool( &httpBench.Secure, secure, "L7BENCH_TEST_SECURE" )
-    setupBool( &httpBench.InsecureSkipVerify, disable_server_auth, "L7BENCH_TEST_DISABLE_SERVER_AUTH" )
+    setupBool( &httpBench.InsecureSkipVerify, disableServerAuth, "L7BENCH_TEST_DISABLE_SERVER_AUTH" )
 
     setupString( &httpBench.Port, port, "L7BENCH_TEST_PORT" )
     if httpBench.Secure {
@@ -60,12 +68,12 @@ func main( ) {
         }
     }
 
-    setupUint( &httpBench.ConcurrentConns, concurrent_conns, "L7BENCH_TEST_CONCURRENT_CONNS" )
+    setupUint( &httpBench.ConcurrentConns, concurrentConns, "L7BENCH_TEST_CONCURRENT_CONNS" )
     setupUint( &httpBench.ConnReqs, conn_reqs, "L7BENCH_TEST_CONN_REQS" )
 
     setupDuration( &httpBench.Duration, duration, "L7BENCH_TEST_DURATION" )
-    setupDuration( &httpBench.ConnReqInterval, conn_req_intvl, "L7BENCH_TEST_CONN_REQ_INTERVAL" )
-    setupDuration( &httpBench.IdleTimeout, idle_timeout, "L7BENCH_TEST_IDLE_TIMEOUT" )
+    setupDuration( &httpBench.ConnReqInterval, connReqIntvl, "L7BENCH_TEST_CONN_REQ_INTERVAL" )
+    setupDuration( &httpBench.IdleTimeout, idleTimeout, "L7BENCH_TEST_IDLE_TIMEOUT" )
 
     glog.Infof( "Starting l7bench test %+v", httpBench )
     httpBench.Start( )
